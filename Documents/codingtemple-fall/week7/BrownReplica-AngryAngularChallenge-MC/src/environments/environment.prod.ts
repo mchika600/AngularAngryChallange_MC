857b2208a25bcb76442b184ee3f2028dd76d1edd
mchika600 <three_stugies@yahoo.com>
2019-10-25 10:54:57 -0500

    Angular Angry Challange Project

---
+++ Documents/codingtemple-fall/week7/BrownReplica-AngryAngularChallenge-MC/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "API KEY",
+    authDomain: "angularangrychallange-mc.firebaseapp.com",
+    databaseURL: "https://angularangrychallange-mc.firebaseio.com",
+    projectId: "angularangrychallange-mc",
+    storageBucket: "angularangrychallange-mc.appspot.com",
+    messagingSenderId: "900107796177",
+    appId: "1:900107796177:web:5ff7ab0e655082cbd28ff0"
+  }
+};
